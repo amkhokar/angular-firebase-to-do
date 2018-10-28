955b20b152e1b130968e4594eafcf863e82c37cf
Ahmed Khokar <amkhokar@gmail.com>
2018-10-27 18:04:04 -0700

    deploy

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'root-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  title = 'to-do';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  selectedTask: Task = null;
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  addTask(newTask: Task) {
+    this.masterTaskList.push(newTask);
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+}
